edd46bd5f715d8845e513e1e971474d889403564
Thomas Guo <thomas.guo@oracle.com>
2016-11-15 15:43:55 +0800

    add instance staff in JCSClient.java

--- services/jcs/src/main/java/cloud/oracle/paas/jcs/JCSClient.java
+++ services/jcs/src/main/java/cloud/oracle/paas/jcs/JCSClient.java
@@ -1,8 +1,61 @@
 package cloud.oracle.paas.jcs;
 
+import cloud.oracle.paas.jcs.apis.ServiceInstances;
+import cloud.oracle.paas.model.HTTPResult;
+import cloud.oracle.paas.util.HTTPRequester;
+
+import java.util.logging.Level;
+
 /**
- * Created by thguo
+ * Client for JCS
  */
 public class JCSClient {
+
+    private static final HTTPRequester requester = new HTTPRequester();
+    private ServiceInstances si;
+
+    public JCSClient() {
+    }
+
+    public  void enableLog(Level level) {
+        requester.enableLogger(level);
+    }
+
+    public HTTPResult createInstance(String serviceName, String payload){
+        return new ServiceInstances(requester).createInstance(serviceName,payload);
+    }
+
+    public HTTPResult viewAllInstancse(){
+        return new ServiceInstances(requester).viewAllInstances();
+    }
+
+    public class Instance{
+        public Instance() {
+            si = new ServiceInstances(requester);
+        }
+
+        public HTTPResult create(String serviceName, String payload){
+            return si.createInstance(serviceName,payload);
+        }
+
+        public HTTPResult viewAll(){
+            return si.viewAllInstances();
+        }
+
+        public HTTPResult view(String sericeName){
+            return si.viewInstance(sericeName);
+        }
+
+        public HTTPResult control(String sericeName, String payload){
+            return si.stopAndStartInstance(sericeName, payload);
+        }
+
+        public HTTPResult delete(String sericeName, String payload){
+            return si.deleteInstance(sericeName, payload);
+        }
+
+    }
+
+
 
 }
